8c66f4602b4713b52bacc8abe85a06571106d88b
karthik chandrashekar <karthik.c@ltts.com>
2020-10-13 13:56:06 +0530

    amq initial commit

---
+++ src/main/java/amq/Consumer.java
@@ -0,0 +1,39 @@
+package amq;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+
+public class Consumer {
+
+	public static void main(String[] args) {
+		Connection connection;
+		try {
+			ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+			connection = factory.createConnection();
+			connection.start();
+			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			Destination queue = session.createQueue("Queue");
+			MessageConsumer consumer = session.createConsumer(queue);
+			Message message = consumer.receive(1000);
+			System.out.println("Message is :"+message);
+			if (message instanceof TextMessage) {
+				TextMessage textMessage = (TextMessage) message;
+				String text = textMessage.getText();
+				System.out.println("Consumer Received: " + text);
+			}
+			session.close();
+			connection.close();
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/main/java/amq/Producer.java
@@ -0,0 +1,37 @@
+package amq;
+
+import javax.jms.Connection;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Producer {
+
+	public static void main(String[] args) {
+		ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+		Connection connection;
+		try {
+			connection = factory.createConnection();
+			connection.start();
+			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			Destination queue = session.createQueue("Queue");
+			MessageProducer producer = session.createProducer(queue);
+			producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+			String msg = "Hello World";
+			TextMessage message = session.createTextMessage(msg);
+			System.out.println("Producer Sent: " + msg);
+			producer.send(message);
+			session.close();
+			connection.close();
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
